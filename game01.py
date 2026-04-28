49f39b176b6bcb538c9a4d81f37a5f954d1f0ac8
Eduarda Menna <eduarda.menna@escola.pr.gov.br>
2026-04-27 21:51:01 -0300

    Update game01.py

---
+++ game01.py
@@ -0,0 +1,22 @@
+import pygame
+
+# Iniciando o pygame, *init* é iniciar.
+pygame.init()
+
+# Definir o tamanho da janela. *display.set_mode é para "ditar" o modo da janela
+WIDTH, HEIGHT = 720, 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption ("Nome da Janela")
+
+# Loop principal do jogo
+running = True
+while running:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+        running = False
+
+    # Atualizando a tela
+    pygame.display.flip()
+
+# Finalizando o pygame
+pygame.quit()
